ada7d65fee6029abb6753d5118541558242e2bbd
John Young <john@skycatchfire.com>
2021-05-30 20:19:39 -0400

    💄 adds drink card

---
+++ components/DrinkCard.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({});
+
+export class DrinkCard extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Dink</Text>
+      </View>
+    );
+  }
+}
