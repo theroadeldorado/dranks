ea18632091364bde551af83cad33511b8104d23d
John Young <john@skycatchfire.com>
2021-05-30 22:08:51 -0400

    💄 starts on card

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './components/Login';
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import theStore from './store/store';
-import CreateAccount from './components/CreateAccount';
 import DrinkCard from './components/DrinkCard';
+import Login from './components/Login';
+import CreateAccount from './components/CreateAccount';
+import { useFonts } from 'expo-font';
+
+import { Nunito_200ExtraLight, Nunito_300Light, Nunito_400Regular, Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold, Nunito_900Black } from '@expo-google-fonts/nunito';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Nunito_200ExtraLight,
+    Nunito_300Light,
+    Nunito_400Regular,
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+    Nunito_900Black,
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Provider store={theStore}>
       <View style={styles.container}>
         <StatusBar style="auto" />
         {/* <Login></Login>
         <CreateAccount></CreateAccount> */}
-        <DrinkCard></DrinkCard>
+        <DrinkCard lightColor="#ffe7d4" darkColor="#8b6c52"></DrinkCard>
+        <DrinkCard lightColor="#deffd4" darkColor="#3e9823"></DrinkCard>
+        <DrinkCard lightColor="#d4d8ff" darkColor="#4a52a2"></DrinkCard>
+        <DrinkCard lightColor="#ffd4e1" darkColor="#8f2e4c"></DrinkCard>
       </View>
     </Provider>
   );
@@ -26,5 +45,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20,
   },
 });
--- components/DrinkCard.js
+++ components/DrinkCard.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import * as Svg from 'react-native-svg';
+import Drink from '../assets/svgs/drink';
 
 function DrinkCard(props) {
+  const { lightColor, darkColor } = props;
   return (
-    <View>
-      <Text>Drink</Text>
+    <View style={{ marginHorizontal: 10, marginBottom: 30 }}>
+      <View style={{ paddingHorizontal: 30, paddingTop: 30, paddingBottom: 15, width: '100%' }}>
+        <View style={{ width: '100%', flexDirection: 'row', marginBottom: 20, alignItems: 'center' }}>
+          <Drink />
+          <View style={{ paddingLeft: 20 }}>
+            <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 6, fontFamily: 'Nunito_600SemiBold' }}>Old Fashioned</Text>
+            <Text style={{ fontSize: 16, fontFamily: 'Nunito_400Regular' }} numberOfLines={2}>
+              One of the great classic bourbon cocktails.
+            </Text>
+          </View>
+        </View>
+        <View style={{ flexDirection: 'row', width: '100%' }}>
+          <View style={{ paddingHorizontal: 8, paddingVertical: 4, borderRadius: 999, backgroundColor: darkColor, marginRight: 8 }}>
+            <Text style={{ fontSize: 12, textTransform: 'uppercase', color: '#fff', fontFamily: 'Nunito_400Regular' }}>Bourbon Whiskey</Text>
+          </View>
+          <View style={{ paddingHorizontal: 8, paddingVertical: 4, borderRadius: 999, backgroundColor: darkColor, marginRight: 8 }}>
+            <Text style={{ fontSize: 12, textTransform: 'uppercase', color: '#fff', fontFamily: 'Nunito_400Regular' }}>Bitters</Text>
+          </View>
+          <View style={{ paddingHorizontal: 8, paddingVertical: 4, borderRadius: 999, backgroundColor: darkColor, marginRight: 8 }}>
+            <Text style={{ fontSize: 12, textTransform: 'uppercase', color: '#fff', fontFamily: 'Nunito_400Regular' }}>Cherry</Text>
+          </View>
+        </View>
+      </View>
+      <View
+        style={{
+          transform: [{ rotate: '1deg' }],
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          borderWidth: 1,
+          borderColor: darkColor,
+          marginRight: 5,
+          borderRadius: 30,
+          zIndex: 0,
+        }}
+      ></View>
+      <View style={{ transform: [{ rotate: '-1deg' }], position: 'absolute', top: 8, left: 8, width: '100%', height: '100%', backgroundColor: lightColor, borderRadius: 30, zIndex: -1 }}></View>
     </View>
   );
 }
